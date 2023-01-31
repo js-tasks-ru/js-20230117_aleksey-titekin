13127c838fa83b1cd123b01bc81aa02d31eda549
Aleksey Titekin <Titekin.A@yandex.ru>
2023-02-01 00:21:22 +0300

    first step - create template component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,46 @@
 export default class NotificationMessage {
+    static activeElement = '';
+
+    constructor(message = '', {duration = 0, type = ''} = {}) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+
+        this.render();
+    }
+
+    render() {
+        let element = document.createElement('div');
+        element.innerHTML = `
+        <div class="notification ${this.type}" style="--value:${this.duration/1000}s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+        </div>
+        `;
+
+        this.element = element.firstElementChild;
+    }
+
+    destroy() {
+        this.remove();
+        this.element = '';
+    }
+
+    remove() {
+        NotificationMessage.activeElement= '';
+        this.element.remove();
+    }
+
+    show() {
+        
+        NotificationMessage.activeElement = '';
+        document.body.append(this.element);
+        setTimeout(() => this.remove(), this.duration);
+    }
 
 }
