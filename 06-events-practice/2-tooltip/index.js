bc89b88a303aad0b45790fe3d3cdb4054d5b9e7d
Aleksey Titekin <titekin.a@yandex.ru>
2023-02-06 15:29:33 +0300

    tooltip completed

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,67 @@
 class Tooltip {
-  initialize () {
+  static #onlyInstance = null;
+  message = "";
+  element = null;
 
+  onOver = (event) => {
+    const elem = event.target.closest('[data-tooltip]');
+    if (!elem) return;
+    this.message = elem.dataset.tooltip;
+    elem.addEventListener("pointermove", this.onMove);
+    elem.addEventListener("pointerout", this.onOut, { once: true });
+    this.render();
+    this.visibleTooltip(true);
+  };
+
+  onOut = (event) => {
+    const elem = event.target.closest('[data-tooltip]');
+    if (!elem) return;
+    elem.removeEventListener("pointermove", this.onMove);
+    this.visibleTooltip(false);
+    this.remove();
+  };
+
+  onMove = (event) => {
+    this.element.style.left = event.clientX + "px";
+    this.element.style.top = event.clientY + "px";
+  };
+
+  constructor() {
+    if (!Tooltip.#onlyInstance) {
+      Tooltip.#onlyInstance = this;
+    } else {
+      return Tooltip.#onlyInstance;
+    }
+  }
+
+  initialize() {
+    document.body.addEventListener("pointerover", this.onOver);
+  }
+
+  render() {
+    const wraper = document.createElement("div");
+    wraper.innerHTML = `<div class="tooltip">${this.message}</div>`;
+    console.log(wraper)
+    this.element = wraper.firstElementChild;
+    document.body.append(this.element);
+  }
+
+  visibleTooltip(isVisible = false) {
+    if (this.element) {
+      this.element.hidden = !isVisible;   
+    }
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+      this.element = null;
+    }
+  }
+
+  destroy() {
+    document.body.removeEventListener("pointerover", this.onOver);
+    this.remove();    
   }
 }
 
